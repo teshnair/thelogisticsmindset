6981c2c6d0ca58b184b96410bcdc6fd1813380ad
teshnair <teshnair@me.com>
2026-09-06 09:21:26 -0400

    Remove recursive HTS question watcher

--- js/fx-ticker.js
+++ js/fx-ticker.js
@@ -135,12 +135,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 // and Chapter 99 result rendering continues to work.
 if (/\/hts-duty-calculator\.html$/i.test(window.location.pathname)) {
   const screeningScript = document.createElement("script");
-  screeningScript.src = "js/hts-import-screening.js?v=20260906-4";
+  screeningScript.src = "js/hts-import-screening.js?v=20260906-5";
   screeningScript.async = false;
   document.head.appendChild(screeningScript);
-
-  const questionGatingScript = document.createElement("script");
-  questionGatingScript.src = "js/hts-question-gating.js?v=20260906-1";
-  questionGatingScript.async = false;
-  document.head.appendChild(questionGatingScript);
 }
